eda1fa3839ce928b9c90e45957d28665606f1a8a
cjlworld <cjlworld@outlook.com>
2023-05-31 22:24:30 +0800

    tic-tac-toe baseline

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, session
+import tictactoe
+import json
+
+app = Flask(__name__)
+app.secret_key = 'fefenlwafmekjsfewfojflkwanfkrjefnjkrea'
+
+# 游戏模式
+class Mode():
+    """
+    游戏模式
+        link: 相对根地址的链接
+        title: 模式的名称
+    """
+    def __init__(self, title, link):
+        self.title = title
+        self.link = link
+
+Modes = [Mode("Tic-Tac-Toe", "tic-tac-toe")]
+
+# 主页面
+@app.route("/")
+def index():
+    """
+    渲染模式按钮
+    """
+    return render_template("index.html", modes=Modes)
+
+# tic-tac-toe
+@app.route("/tic-tac-toe", methods=["GET", "POST"])
+def TicTacToe():
+    session["board"] = tictactoe.initial_state()
+    return render_template("tictactoe.html")
+
+@app.route("/tic-tac-toe/action", methods=["POST"])
+def tictactoe_action():
+    """
+    receive the actions from the frontend
+    """
+    json_data = request.json
+    row = int(json_data["row"])
+    col = int(json_data["col"])
+    if row < 0 or row > 2 or col < 0 or col > 2:
+        return json.dumps({"board": board, "signal": "failure"})
+    board = session["board"]
+    if tictactoe.player(board) != tictactoe.X:
+        return json.dumps({"board": board, "signal": "failure"})
+    board[row][col] = tictactoe.X
+    board = tictactoe.result(board, tictactoe.minimax(board))
+    session["board"] = board
+    return json.dumps({"board": board, "signal": "success"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
